3c6614617c4b4f48630e07da150274942dd12437
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 07:17:55 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,16 +16,4 @@ def run_guessing_game
     end
   end
   puts "Goodbye!"
-  num = rand(1..6)
-  input = gets.chomp
-  if input == "exit"
-    puts "Goodbye!"
-  elsif input.to_i == num
-    puts "You guessed the correct number!"
-  elsif input.to_i != num 
-    puts "The computer guessed #{num}."
-  else 
-    "Invalid number!"
-  end 
- 
 end 
